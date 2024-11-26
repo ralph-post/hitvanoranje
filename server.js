478bf55d2574ee73e0f1f5184936ec7d25a9b983
Ralph Post <ralph@ralphpost.com>
2024-11-26 21:22:12 +0100

    DO settings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Serve static files from the root directory
 app.use(express.static('./'));
@@ -9,6 +9,10 @@ app.use(express.static('./'));
 // Handle both root and callback routes by serving index.html
 app.get(['/', '/callback'], (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
 });
 
 app.listen(port, () => {
